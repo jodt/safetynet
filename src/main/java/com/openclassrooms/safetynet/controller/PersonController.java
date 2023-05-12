0a6339492fcee9d3281be4f5006641235b894a91
joel Dumortier <joel.dumortier@gmail.com>
2023-05-12 21:45:03 +0200

    add logs

--- src/main/java/com/openclassrooms/safetynet/controller/PersonController.java
+++ src/main/java/com/openclassrooms/safetynet/controller/PersonController.java
@@ -1,19 +1,22 @@
 package com.openclassrooms.safetynet.controller;
 
+import com.openclassrooms.safetynet.exception.MailsNotFoundException;
 import com.openclassrooms.safetynet.exception.PersonNotFoundException;
 import com.openclassrooms.safetynet.model.Person;
 import com.openclassrooms.safetynet.repository.PersonRepository;
 import com.openclassrooms.safetynet.service.PersonService;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpResponse;
 import java.util.List;
+import org.slf4j.Logger;
+
 
 @RestController
 public class PersonController {
-
+    private final Logger logger = LoggerFactory.getLogger(PersonController.class);
     private final PersonRepository personRepository;
     private final PersonService personService;
 
@@ -23,30 +26,35 @@ public class PersonController {
     }
 
     @GetMapping("/")
-    public List<Person>getPersons() throws Exception {
+    public List<Person>getPersons() {
+        logger.info("Start process to collect all persons");
         return this.personRepository.getPersons();
     }
 
     @PostMapping("/person")
-    public ResponseEntity<Person> addPerson(@RequestBody Person person){
+    public ResponseEntity<Person> addPerson(@RequestBody Person person) {
         this.personService.addPerson(person);
+        logger.info("Start process to add a new person {} {}", person.getFirstName(), person.getLastName());
         return new ResponseEntity<>(person, HttpStatus.CREATED);
     }
 
     @PutMapping("/person")
-    public ResponseEntity<?> updatePerson(@RequestBody Person person) throws PersonNotFoundException {
+    public ResponseEntity<Object> updatePerson(@RequestBody Person person) throws PersonNotFoundException {
         this.personService.updatePerson(person);
+        logger.info("Start process to update the person with firstname {} and lastname {}", person.getFirstName(), person.getLastName());
         return new ResponseEntity<>(person, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/person")
-    public ResponseEntity<String> deletePerson(@RequestParam String firstName, @RequestParam String lastName) throws Exception {
+    public ResponseEntity<Object> deletePerson(@RequestParam String firstName, @RequestParam String lastName) throws Exception {
         this.personService.deletePerson(firstName,lastName);
-        return new ResponseEntity<>("Person deleted", HttpStatus.NO_CONTENT);
+        logger.info("Start process to delete the person: {} {}", firstName,lastName);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/communityEmail")
-    public List<String> getCommunityEmail(@RequestParam String city){
+    public List<String> getCommunityEmail(@RequestParam String city) throws MailsNotFoundException {
+        logger.info("Start process to collect all people's emails for the city {}", city);
         return this.personService.getMailsByCity(city);
     }
 }
--- src/main/java/com/openclassrooms/safetynet/service/PersonService.java
+++ src/main/java/com/openclassrooms/safetynet/service/PersonService.java
@@ -1,16 +1,20 @@
 package com.openclassrooms.safetynet.service;
 
 import com.openclassrooms.safetynet.exception.PersonNotFoundException;
+import com.openclassrooms.safetynet.exception.MailsNotFoundException;
 import com.openclassrooms.safetynet.model.Person;
 import com.openclassrooms.safetynet.repository.PersonRepository;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
 
 @Service
 public class PersonService {
 
+    private final Logger logger = LoggerFactory.getLogger(PersonService.class);
     private final PersonRepository personRepository;
 
     public PersonService(PersonRepository personRepository) {
@@ -18,13 +22,18 @@ public class PersonService {
     }
 
 
-    public List<String> getMailsByCity(String city){
+    public List<String> getMailsByCity(String city) throws MailsNotFoundException {
+        logger.debug("try to collect all people's mail of the city: {}", city);
         List<String> mails = new ArrayList<>();
         List<Person> persons = this.personRepository.getPersons();
         for(Person person : persons) {
             if (person.getCity().equals(city)){
                 mails.add(person.getEmail());
             }
+        }
+        if(mails.isEmpty()){
+            logger.error("No email found for people living in {}", city);
+            throw new MailsNotFoundException("Aucun mail trouvé pour la ville " + city);
         }
         return mails;
     }
@@ -45,8 +54,10 @@ public class PersonService {
     }
 
     private Person findPersonByFirstNameAndLastName(String firstName, String lastName) throws PersonNotFoundException {
+        logger.debug("Try to find person with firstname {} and lastname {}", firstName, lastName);
         Person personFound = this.personRepository.findPersonByFirstNameAndLastName(firstName,lastName);
         if(personFound == null){
+            logger.error("Person not found with firstname {} and lastname {}", firstName, lastName);
             throw new PersonNotFoundException("Personne non trouvée avec le prénom " + firstName + " et le nom "+ lastName) ;
         }
         return personFound;
