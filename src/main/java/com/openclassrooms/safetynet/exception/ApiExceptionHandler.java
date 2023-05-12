1b198fc63cb050a5d8953127c74ca66870ad303f
joel Dumortier <joel.dumortier@gmail.com>
2023-05-12 13:51:30 +0200

    create exceptions handler

---
+++ src/main/java/com/openclassrooms/safetynet/exception/ApiExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.openclassrooms.safetynet.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(value = {PersonNotFoundException.class})
+    public ResponseEntity<Object> handlePersonNotFoundException(PersonNotFoundException e){
+        ApiException apiException = new ApiException(
+                e.getMessage(),
+                HttpStatus.NOT_FOUND,
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(apiException,HttpStatus.NOT_FOUND);
+    }
+
+}
---
+++ src/main/java/com/openclassrooms/safetynet/exception/PersonNotFoundException.java
@@ -0,0 +1,7 @@
+package com.openclassrooms.safetynet.exception;
+
+public class PersonNotFoundException extends Exception {
+    public PersonNotFoundException(String message) {
+        super(message);
+    }
+}
