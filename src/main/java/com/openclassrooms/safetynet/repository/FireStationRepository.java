aa1b25dc0061cd46a3e572cecd6e6777b3cc37b9
joel Dumortier <joel.dumortier@gmail.com>
2023-05-16 22:42:18 +0200

    add methods to FireStationRepository

--- src/main/java/com/openclassrooms/safetynet/repository/FireStationRepository.java
+++ src/main/java/com/openclassrooms/safetynet/repository/FireStationRepository.java
@@ -18,4 +18,38 @@ public class FireStationRepository {
     public void setFireStations(List<FireStation> fireStations) {
         this.firestations = fireStations;
     }
+
+    public FireStation addFireStation(FireStation fireStation) {
+        this.firestations.add(fireStation);
+        return fireStation;
+    }
+
+    public FireStation getFireStationByNumber(int stationNumber) {
+        FireStation fireStationResult = null;
+        for (FireStation fireStation : this.firestations){
+            if (fireStation.getStation() == stationNumber){
+                fireStationResult = fireStation;
+            }
+        }
+        return fireStationResult;
+    }
+
+    public FireStation getFireStationByAddress(String stationAddress) {
+        FireStation fireStationResult = null;
+        for (FireStation fireStation : this.firestations){
+            if (fireStation.getAddress().equals(stationAddress)){
+                fireStationResult = fireStation;
+            }
+        }
+        return fireStationResult;
+    }
+
+    public void deleteStation (FireStation fireStation){
+        this.firestations.remove(fireStation);
+    }
+
+    public void updateStationNumber (FireStation fireStation){
+        FireStation updateFireStation = this.getFireStationByAddress(fireStation.getAddress());
+        updateFireStation.setStation(fireStation.getStation());
+    }
 }
