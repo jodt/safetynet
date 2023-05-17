cfc5766c81323e1da2dd6cd209522a3812aa5d6c
joel Dumortier <joel.dumortier@gmail.com>
2023-05-17 23:29:26 +0200

    add methods to MedicalRecordRepository

--- src/main/java/com/openclassrooms/safetynet/repository/MedicalRecordRepository.java
+++ src/main/java/com/openclassrooms/safetynet/repository/MedicalRecordRepository.java
@@ -19,4 +19,30 @@ public class MedicalRecordRepository {
         this.medicalRecords = medicalRecords;
     }
 
+    public MedicalRecord addMedicalRecord (MedicalRecord medicalRecord){
+        this.medicalRecords.add(medicalRecord);
+        return medicalRecord;
+    }
+
+    public void deleteMedicalRecord (MedicalRecord medicalRecord){
+        this.medicalRecords.remove(medicalRecord);
+    }
+
+    public void updateMedicalRecord(MedicalRecord medicalRecord){
+        MedicalRecord updateMedicalRecord = this.findMedicalRecordByFirstNameAndLastName(medicalRecord.getFirstName(),medicalRecord.getLastName());
+        updateMedicalRecord.setBirthdate(medicalRecord.getBirthdate());
+        updateMedicalRecord.setAllergies(medicalRecord.getAllergies());
+        updateMedicalRecord.setMedications(medicalRecord.getMedications());
+    }
+
+    public MedicalRecord findMedicalRecordByFirstNameAndLastName(String firstName, String lastName) {
+        MedicalRecord medicalRecordResult = null;
+        for (MedicalRecord medicalRecord : this.medicalRecords){
+            if (medicalRecord.getFirstName().toLowerCase().equals(firstName.toLowerCase()) && medicalRecord.getLastName().toLowerCase().equals(lastName.toLowerCase())){
+                medicalRecordResult = medicalRecord;
+            }
+        }
+        return medicalRecordResult;
+    }
+
 }
