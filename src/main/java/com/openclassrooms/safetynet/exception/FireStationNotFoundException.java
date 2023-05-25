49f0c67d600d552948e6a992786d8f3def6f0aa8
joel Dumortier <joel.dumortier@gmail.com>
2023-05-25 09:18:07 +0200

    fix bug: create FireStatioNotFoundException

---
+++ src/main/java/com/openclassrooms/safetynet/exception/FireStationNotFoundException.java
@@ -0,0 +1,8 @@
+package com.openclassrooms.safetynet.exception;
+
+public class FireStationNotFoundException extends Exception {
+
+    public FireStationNotFoundException(String message) {
+        super(message);
+    }
+}
