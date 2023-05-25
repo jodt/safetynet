b8256feeb036a28325fe91c10aabd0da2f67c023
jodt <joel.dumortier@gmail.com>
2023-05-25 09:01:49 +0200

    add logs

--- src/main/java/com/openclassrooms/safetynet/controller/MedicalRecordController.java
+++ src/main/java/com/openclassrooms/safetynet/controller/MedicalRecordController.java
@@ -4,6 +4,8 @@ import com.openclassrooms.safetynet.exception.MedicalRecordNotFoundException;
 import com.openclassrooms.safetynet.model.MedicalRecord;
 import com.openclassrooms.safetynet.repository.MedicalRecordRepository;
 import com.openclassrooms.safetynet.service.MedicalRecordService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/medicalRecord")
 public class MedicalRecordController {
+
+    private final Logger logger = LoggerFactory.getLogger(MedicalRecordController.class);
     private final MedicalRecordRepository medicalRecordRepository;
     private final MedicalRecordService medicalRecordService;
 
@@ -23,24 +27,31 @@ public class MedicalRecordController {
 
     @GetMapping()
     public List<MedicalRecord> getMedicalRecords(){
+        logger.info("Start process to collect all Medical Records");
         return medicalRecordRepository.getMedicalRecords();
     }
 
     @PostMapping()
     public ResponseEntity<MedicalRecord> addMedicalRecord(@RequestBody MedicalRecord medicalRecord){
+        logger.info("Start process to add a new medical record for {} {}", medicalRecord.getFirstName(), medicalRecord.getLastName());
         this.medicalRecordService.addMedicalRecord(medicalRecord);
+        logger.info("Process end successfully");
         return new ResponseEntity<>(medicalRecord, HttpStatus.CREATED);
     }
 
     @PutMapping()
     public ResponseEntity<MedicalRecord> updateMedicalRecord(@RequestBody MedicalRecord medicalRecord) throws MedicalRecordNotFoundException {
+        logger.info("Start process to update the medical record of {} {}", medicalRecord.getFirstName(),medicalRecord.getLastName());
         this.medicalRecordService.updateMedicalrecord(medicalRecord);
+        logger.info("Process end successfully");
         return new ResponseEntity<>(medicalRecord, HttpStatus.OK);
     }
 
     @DeleteMapping()
     public ResponseEntity<Object> deleteMedicalrecord(@RequestParam String firstName, @RequestParam String lastName) throws MedicalRecordNotFoundException {
+        logger.info("Start process to delete the medical record of {} {}", firstName, lastName);
         this.medicalRecordService.deleteMedicalRecord(firstName,lastName);
+        logger.info("Process end successfully");
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/openclassrooms/safetynet/service/MedicalRecordService.java
+++ src/main/java/com/openclassrooms/safetynet/service/MedicalRecordService.java
@@ -3,12 +3,16 @@ package com.openclassrooms.safetynet.service;
 import com.openclassrooms.safetynet.exception.MedicalRecordNotFoundException;
 import com.openclassrooms.safetynet.model.MedicalRecord;
 import com.openclassrooms.safetynet.repository.MedicalRecordRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class MedicalRecordService {
+
+    private final Logger logger = LoggerFactory.getLogger(MedicalRecordService.class);
 
     private final MedicalRecordRepository medicalRecordRepository;
 
@@ -17,26 +21,36 @@ public class MedicalRecordService {
     }
 
     public MedicalRecord addMedicalRecord(MedicalRecord medicalRecord){
+        logger.debug("Try to add a new medical record {} {}", medicalRecord.getFirstName(), medicalRecord.getLastName());
         this.medicalRecordRepository.addMedicalRecord(medicalRecord);
+        logger.debug("Medical record successfully added");
         return medicalRecord;
     }
 
     public MedicalRecord updateMedicalrecord(MedicalRecord medicalRecord) throws MedicalRecordNotFoundException {
+        logger.debug("Try to update the medical record of {} {}", medicalRecord.getFirstName(), medicalRecord.getLastName());
         this.findMedicalRecordByFirstNameAndLastName(medicalRecord.getFirstName(), medicalRecord.getLastName());
         this.medicalRecordRepository.updateMedicalRecord(medicalRecord);
+        logger.debug("Medical record successfully updated");
         return medicalRecord;
     }
 
     public void deleteMedicalRecord(String firstName, String lastName) throws MedicalRecordNotFoundException {
+        logger.debug("Try to delete the medical record of {} {}", firstName, lastName);
         MedicalRecord medicalRecordFound = this.findMedicalRecordByFirstNameAndLastName(firstName, lastName);
+        logger.debug("Medical record successfully deleted");
         this.medicalRecordRepository.deleteMedicalRecord(medicalRecordFound);
     }
 
     private MedicalRecord findMedicalRecordByFirstNameAndLastName(String firstName, String lastName) throws MedicalRecordNotFoundException {
+        logger.debug("Try to find the medical record of {} {}", firstName, lastName);
         MedicalRecord medicalRecordFound = this.medicalRecordRepository.findMedicalRecordByFirstNameAndLastName(firstName,lastName);
         if (medicalRecordFound == null){
+            logger.error("Medical record not found for {} {}", firstName, lastName);
             throw new MedicalRecordNotFoundException("Medical record not found for " + firstName + " " + lastName);
         }
+        logger.debug("Medical record found for {} {}", firstName, lastName);
         return medicalRecordFound;
     }
+qq
 }
