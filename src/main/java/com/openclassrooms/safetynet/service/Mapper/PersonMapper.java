9b6fd26a8d1b123784dae664b19e1f1cf40f2a49
joel Dumortier <joel.dumortier@gmail.com>
2023-06-02 15:44:27 +0200

    add methods

--- src/main/java/com/openclassrooms/safetynet/service/Mapper/PersonMapper.java
+++ src/main/java/com/openclassrooms/safetynet/service/Mapper/PersonMapper.java
@@ -1,9 +1,20 @@
 package com.openclassrooms.safetynet.service.Mapper;
 
-import com.openclassrooms.safetynet.dto.PersonWithAgeDTO;
+import com.openclassrooms.safetynet.dto.PersonInfoDTO;
+import com.openclassrooms.safetynet.dto.PersonWithAddressAndPhoneDTO;
+import com.openclassrooms.safetynet.dto.PersonWithAgeAndFamilyMembersDTO;
+import com.openclassrooms.safetynet.dto.PersonWithMedicalRecordDTO;
 import com.openclassrooms.safetynet.model.MedicalRecord;
+import com.openclassrooms.safetynet.model.Person;
 
 public interface PersonMapper {
 
-    PersonWithAgeDTO asPersonWithAgeDTO (MedicalRecord medicalRecord);
+    PersonWithAgeAndFamilyMembersDTO asPersonWithAgeDTO (MedicalRecord medicalRecord);
+
+    PersonWithAddressAndPhoneDTO asPersonWithAddressAndPhoneDTO (Person person,MedicalRecord medicalRecord);
+
+    PersonWithMedicalRecordDTO asPersonWithMedicalRecordDTO (Person person, MedicalRecord medicalRecord);
+
+    PersonInfoDTO asPersonInfoDTO(Person person, MedicalRecord medicalRecord);
+
 }
