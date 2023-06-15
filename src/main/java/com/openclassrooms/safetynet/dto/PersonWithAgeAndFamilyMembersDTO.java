1476abcb8a3719d1e04408acd57d5323bbab71ba
joel Dumortier <joel.dumortier@gmail.com>
2023-06-15 11:42:41 +0200

    add lombok annotations

--- src/main/java/com/openclassrooms/safetynet/dto/PersonWithAgeAndFamilyMembersDTO.java
+++ src/main/java/com/openclassrooms/safetynet/dto/PersonWithAgeAndFamilyMembersDTO.java
@@ -1,10 +1,16 @@
 package com.openclassrooms.safetynet.dto;
 
 import com.openclassrooms.safetynet.model.Person;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class PersonWithAgeAndFamilyMembersDTO {
 
