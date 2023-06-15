fdede558e86cfe170b5fa7b32d3a9ca1cbf208ae
joel Dumortier <joel.dumortier@gmail.com>
2023-06-15 11:35:01 +0200

    add lombok annotations

--- src/main/java/com/openclassrooms/safetynet/model/MedicalRecord.java
+++ src/main/java/com/openclassrooms/safetynet/model/MedicalRecord.java
@@ -5,11 +5,17 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class MedicalRecord {
 
