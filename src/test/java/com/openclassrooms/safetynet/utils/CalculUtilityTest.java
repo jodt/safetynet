9d83949a21d45b5bad744a117a9596791255d80f
joel Dumortier <joel.dumortier@gmail.com>
2023-06-16 11:22:27 +0200

    add test for calculateAgeOfPerson method

--- src/test/java/com/openclassrooms/safetynet/utils/CalculUtilityTest.java
+++ src/test/java/com/openclassrooms/safetynet/utils/CalculUtilityTest.java
@@ -1,24 +1,31 @@
 package com.openclassrooms.safetynet.utils;
-;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class CalculUtilityTest {
 
-    LocalDate birthdate = LocalDate.of(2020,01,01);
+    LocalDate birthdate = LocalDate.of(2020, 01, 01);
 
     @DisplayName("should get the person age")
     @Test
     void shouldCalculateAgeOfPerson() {
         int age = CalculUtility.calculateAgeOfPerson(birthdate);
 
-        assertEquals(3,age);
+        assertEquals(3, age);
+    }
+
+    @DisplayName("should not get the person age but 0")
+    @Test
+    void shouldNotCalculateAgeOfPerson() {
+        int age = CalculUtility.calculateAgeOfPerson(null);
+
+        assertEquals(0, age);
     }
 
 }
