8cef449d943b3a0a361a60522556bb46f1e55596
joel Dumortier <joel.dumortier@gmail.com>
2023-06-24 22:27:35 +0200

    add lombok annotations

--- src/main/java/com/openclassrooms/safetynet/dto/FireDTO.java
+++ src/main/java/com/openclassrooms/safetynet/dto/FireDTO.java
@@ -1,12 +1,16 @@
 package com.openclassrooms.safetynet.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Data
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class FireDTO {
     private List<PersonWithMedicalRecordDTO> personsListInFireCase;
     private int fireStationNumber;
