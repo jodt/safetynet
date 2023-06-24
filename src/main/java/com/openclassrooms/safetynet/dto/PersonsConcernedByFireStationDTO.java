4c743c788b434dfd08b412bf5fcfcefc5e5746c5
joel Dumortier <joel.dumortier@gmail.com>
2023-06-24 22:28:13 +0200

    add lombok annotations

--- src/main/java/com/openclassrooms/safetynet/dto/PersonsConcernedByFireStationDTO.java
+++ src/main/java/com/openclassrooms/safetynet/dto/PersonsConcernedByFireStationDTO.java
@@ -1,13 +1,17 @@
 package com.openclassrooms.safetynet.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
-@Data
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class PersonsConcernedByFireStationDTO {
     private List<PersonWithAddressAndPhoneDTO> personWithAddressAndPhoneDTOList;
     private AtomicInteger children;
