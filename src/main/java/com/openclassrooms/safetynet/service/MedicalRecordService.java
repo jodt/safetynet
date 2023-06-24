d0382a36a09293f41adf9327fd8720048f80ac8e
joel Dumortier <joel.dumortier@gmail.com>
2023-06-24 22:17:54 +0200

    add method to get all medical records

--- src/main/java/com/openclassrooms/safetynet/service/MedicalRecordService.java
+++ src/main/java/com/openclassrooms/safetynet/service/MedicalRecordService.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -19,6 +20,10 @@ public class MedicalRecordService {
 
     public MedicalRecordService(MedicalRecordRepository medicalRecordRepository) {
         this.medicalRecordRepository = medicalRecordRepository;
+    }
+
+    public List<MedicalRecord> getAllMedicalRecords() {
+        return this.medicalRecordRepository.getMedicalRecords();
     }
 
     public MedicalRecord addMedicalRecord(MedicalRecord medicalRecord) {
