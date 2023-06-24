fe07b747300a6bf0a0966cdd91c7e60731dbe836
joel Dumortier <joel.dumortier@gmail.com>
2023-06-24 22:30:00 +0200

    add tests

--- src/test/java/com/openclassrooms/safetynet/controller/AlertControllerTest.java
+++ src/test/java/com/openclassrooms/safetynet/controller/AlertControllerTest.java
@@ -8,6 +8,8 @@ import com.openclassrooms.safetynet.model.Person;
 import com.openclassrooms.safetynet.service.PersonService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,9 +20,10 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -33,6 +36,13 @@ class AlertControllerTest {
 
     @MockBean
     private PersonService personService;
+
+    @Captor
+    ArgumentCaptor<String> addressCaptor;
+
+    @Captor
+    ArgumentCaptor<Integer> stationNumberCaptor;
+
 
     @DisplayName("Should get children list")
     @Test
@@ -64,10 +74,14 @@ class AlertControllerTest {
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].firstName", is("firstnameChild1")));
 
+        verify(this.personService, times(1)).findChildrenByAddress(addressCaptor.capture());
+        String addressCaptorValue = addressCaptor.getValue();
+        assertEquals("1509 Culver St", addressCaptorValue);
+
     }
 
 
-    @DisplayName("Should not get children list -> MedicalRecordNotFoundException")
+    @DisplayName("Should not get children list -> medical record not found")
     @Test
     void shouldNotGetChildrenListMedicalRecordNotFoundException() throws Exception {
 
@@ -76,10 +90,12 @@ class AlertControllerTest {
         this.mockMvc.perform(get("/childAlert").param("address", "1509 Culver St"))
                 .andExpect(status().isNotFound());
 
+        verify(this.personService, times(1)).findChildrenByAddress(anyString());
+
     }
 
 
-    @DisplayName("Should not get children list -> PersonNotFoundException")
+    @DisplayName("Should not get children list -> person not found")
     @Test
     void shouldNotGetChildrenListPersonNotFoundException() throws Exception {
 
@@ -87,6 +103,8 @@ class AlertControllerTest {
 
         this.mockMvc.perform(get("/childAlert").param("address", "1509 Culver St"))
                 .andExpect(status().isNotFound());
+
+        verify(this.personService, times(1)).findChildrenByAddress(anyString());
 
     }
 
@@ -103,10 +121,15 @@ class AlertControllerTest {
                 .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect(jsonPath("$[0]", is("001-001-001")))
                 .andExpect(jsonPath("$[1]", is("001-001-002")));
+
+        verify(this.personService, times(1)).findPhoneNumberByFireStationNumber(stationNumberCaptor.capture());
+        int stationNumberCaptorValue = stationNumberCaptor.getValue();
+        assertEquals(5, stationNumberCaptorValue);
+
     }
 
 
-    @DisplayName("Should not get phone number list -> FireStationNotFoundException")
+    @DisplayName("Should not get phone number list -> fire station not found")
     @Test
     void shouldNotGetPeoplePhoneNumberByFIreStationNumber() throws Exception {
 
@@ -114,6 +137,9 @@ class AlertControllerTest {
 
         this.mockMvc.perform(get("/phoneAlert").param("firestation", "5"))
                 .andExpect(status().isNotFound());
+
+        verify(this.personService, times(1)).findPhoneNumberByFireStationNumber(anyInt());
+
     }
 
 }
